2055170b053982144f7b00ec64265658a33ecd6b
George Lazar <georgelazar@yahoo.com>
2016-03-18 15:02:52 -0700

    PASSED

--- booleans.js
+++ booleans.js
@@ -1,5 +1,3 @@
-//NOT PASSED
-
 /*! strings v0.0.0 - MIT license */
 'use strict';
 
@@ -10,28 +8,28 @@
 
 var booleanChecker = function () {
    // return any boolean value
-   return 
+   return 2 === 2;
 }
 
 var numberEquality = function () {
   // Fix return to return true
     // conssole.log(4234 === 07080);
-  return 4234 === 07080
+  return 4234 !== 07080
 }
 
 var stringEquality = function () {
   // Fix the strings so the functions returns true
-  return 'guilty feet' === 'GUILTY FEET';
+  return 'guilty feet' === 'guilty feet';
 }
 
 var typeChecker = function () {
   // Fix the return statement to return true
-  return typeof 435435345 === 'boolean';
+  return typeof 435435345 === 'number';
 }
 
 var booleanFalsy = function () {
   // Change the values in the parentheses so else statement runs  
-  if (true) {
+  if (2 === 3) {
     return 'I\'m not supposed to be here!';
   } else {
     return 'Here I am, send me an angel!';
@@ -39,8 +37,9 @@ var booleanFalsy = function () {
 }
 
 var booleanTruthy = function () {
-  // Look up the 'negation' operator and apply it to make the body of the if statement pass
-  if (null) {
+  // Look up the 'negation' operator and apply it to make the body 
+  //of the if statement pass
+  if (!null) {
     return 'This is not so null afterall';
   }
 }

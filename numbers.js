c71863b544d276e54fe79b5cce3962d9250f2c48
George Lazar <georgelazar@yahoo.com>
2016-03-18 15:05:26 -0700

    Passed

--- numbers.js
+++ numbers.js
@@ -1,5 +1,3 @@
-//NOT PASSED
-
 /*! strings v0.0.0 - MIT license */
 'use strict';
 
@@ -11,29 +9,30 @@
 
 var numberChecker = function () {
    // return any number below
-   return
+   return 2;
 }
 
 var numberSum = function () {
   // Return the sum of the numbers below
     // 4234 and 07080
-  return
+  return 4234 + 07080;
 }
 
 var numberModulus = function () {
   // Return the remainder of the quotient of the 2 numbers below
     // 11314, 37
-  return
+  return 11314 % 37;
 }
 
 var numberHelper1 = function () {
-  // Find and use a javascript method to convert the string below to a number and return it
+  // Find and use a javascript method to convert the string below to a 
+  //number and return it
     // '8675309'
-  return
+  return Number('8675309');
 }
 
 var numberHelper2 = function () {
   // Convert the number below to a string
     // 8675309
-  return
+  return (8675309).toString();
 }
